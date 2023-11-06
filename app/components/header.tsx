0ec7d717c67b17a2d1e48d89d1e67f9c6f846125
Y3sterd4y <nongboy.chaiwat@gmail.com>
2023-11-06 12:28:56 +0700

    Update: homepage-content

---
+++ app/components/header.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import TeslaIcon from "./icons/TeslaIcon";
+import SupportIcon from "./icons/SupportIcon";
+import EarthIcon from "./icons/EarthIcon";
+import AccountIcon from "./icons/AccountIcon";
+
+export default function Header() {
+  return (
+    <nav className="w-full fixed top-0 z-50">
+      <div className="px-[32px] py-4 grid grid-cols-2 md:grid-cols-3 text-white ">
+        <div className="flex justify-start items-center px-4">
+          <Link href="/">
+            <TeslaIcon />
+          </Link>
+        </div>
+        <div className="max-md:hidden px-4">
+          <ol className="flex justify-center items-center space-x-3">
+            <li className="">
+              <Link
+                className="py-2 px-3 text-sm font-semibold rounded hover:bg-white hover:text-black"
+                href="/"
+              >
+                Vehicles
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                className="py-2 px-3 text-sm font-semibold rounded hover:bg-white hover:text-black"
+                href="/"
+              >
+                Energy
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                className="py-2 px-3 text-sm font-semibold rounded hover:bg-white hover:text-black"
+                href="/"
+              >
+                Charging
+              </Link>
+            </li>
+            <li className="">
+              <Link
+                className="py-2 px-3 text-sm font-semibold rounded hover:bg-white hover:text-black"
+                href="/"
+              >
+                Discover
+              </Link>
+            </li>
+          </ol>
+        </div>
+        <div className="max-md:hidden px-5">
+          <ol className="flex justify-end items-center space-x-3">
+            <li>
+              <Link href="/">
+                <SupportIcon />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <EarthIcon />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <AccountIcon />
+              </Link>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ app/components/section-video.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+
+type Props = {
+  title: string;
+  subtitle: string;
+  bgVideo: string;
+  VideoClass?: string;
+};
+
+export default function SectionVideo({
+  title,
+  subtitle,
+  bgVideo,
+  VideoClass,
+}: Props) {
+  return (
+    <div className="static">
+      <section className="h-[100vh] relative flex flex-col items-center snap-start">
+        <video
+          autoPlay
+          loop
+          muted
+          className={`-z-10 object-cover ${VideoClass}`}
+        >
+          <source src={bgVideo} type="" />
+        </video>
+        <div className="h-full absolute pt-14 flex flex-col justify-between text-center">
+          <div>
+            <article className="mt-8 text-white">
+              <h1 className="text-4xl font-semibold">{title}</h1>
+              <h4 className="text-xl">{subtitle}</h4>
+            </article>
+          </div>
+          <div className=" ">
+            <div className="flex justify-center gap-6 p-8">
+              <Link
+                href=""
+                className="w-[264px] py-2.5 bg-[#171A20CC] text-sm font-semibold rounded"
+              >
+                <span>Order Now</span>
+              </Link>
+              <Link
+                href=""
+                className="w-[264px] py-2.5 bg-[#F4F4F4A6] text-sm font-semibold text-black rounded"
+              >
+                <span>Demo Drive</span>
+              </Link>
+            </div>
+            <div className="mb-4">
+              <p className=" text-xs font-medium">
+                *Excludes taxes and fees with price subject to change. Available
+                in select states. See Details
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
